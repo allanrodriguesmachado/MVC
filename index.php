de679fb8bc1b343f3b81343d7573e50e39cf1bbf
allanrodriguesmachado <allan.rodrigues14@hotmail.com>
2021-08-02 23:47:01 -0300

    atualização

--- index.php
+++ index.php
@@ -1,11 +1,136 @@
-<?php
-ob_start();
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.12.0/devicon.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300&display=swap" rel="stylesheet">
+    <title>Allan Developer</title>
+</head>
+<body>
+    <header >
+      <div class="center">
+          <div class="logo left">
+              <img class="logo-front" src="assets/img/Logo" alt="">
+          </div>
+          <nav class="desktop right">
+              <ul>
+                  <li><a href="">Home</a></li>
+                  <li><a href="">Sobre</a></li>
+                  <li><a href="">Contato</a></li>
+              </ul>
+          </nav>
 
-session_start();
+          <nav class="mobile right">
+              <div class="botao-menu-mobile"><i class="fa fa-bars" aria-hidden="true"></i></div>
+              <ul>
+                  <li><a href="">Home</a></li>
+                  <li><a href="">Sobre</a></li>
+                  <li><a href="">Contato</a></li>
+              </ul>
+          </nav>
+          <div class="clear"></div>
+      </div>
+    </header>
 
-require __DIR__ . "/vendor/autoload.php";
 
-echo "Agora Esta indo tudo certo";
+    <section class="banner-principal">
+        <div class="overlay"></div>
+        <div class="center">
+            <form action="">
+                <h2>Qual seu melhor e-mail</h2>
+                <label>
+                    <input type="email" name="email" required>
+                </label>
+                <input type="submit" name="acao" value="cadastrar">
+            </form>
+        </div>
+    </section>
 
 
-ob_end_flush();
+    <section class="descricao-autor">
+      <div class="center">
+          <div class="w50 left">
+              <h2>Allan Rodrigues</h2>
+              <p>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+              </p>
+              <div class="contato">
+                  <a href="https://github.com/allanrodriguesmachado"><i class="fa fa-github" aria-hidden="true"></i></a>
+                  <a href="https://github.com/allanrodriguesmachado"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                  <a href="https://github.com/allanrodriguesmachado"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+              </div>
+          </div>
+
+          <div class="w50 left">
+              <img class="right" src="assets/img/bg-perfil.png"  style="height: 100%" alt="">
+          </div>
+          <div class="clear"></div>
+      </div>
+    </section>
+
+
+    <section class="especialidades">
+        <div class="center">
+            <h2 class="title">Minhas especialidade</h2>
+
+                <div class="w33 left box-especialidades especialidas-icons">
+                    <img class="especialidas-icons" src="assets/img/php.png">
+                    <img class="especialidas-icons" src="assets/img/laravel.png">
+                    <img class="especialidas-icons" src="assets/img/bootstrap.png">
+                </div>
+
+            <div class="w33 left box-especialidades especialidas-icons">
+                <img class="especialidas-icons" src="assets/img/composer.png" >
+                <img class="especialidas-icons" src="assets/img/composer.png" >
+                <img class="especialidas-icons" src="assets/img/php.png">
+            </div>
+        </div>
+        <div class="clear"></div>
+    </section>
+
+
+<!--    <section class="extras">-->
+<!--        <div class="center">-->
+<!--            <div class="w50 left">-->
+<!--                <h2 class="title">Depoimentos</h2>-->
+<!--                <div class="depoimento-single">-->
+<!--                    <p class="depois-descricao">texto</p>-->
+<!--                    <p class="nome-autor">-->
+<!--                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text-->
+<!--                    </p>-->
+<!--                </div>-->
+<!---->
+<!--                <div class="depoimento-single">-->
+<!--                    <p class="depois-descricao">texto</p>-->
+<!--                    <p class="nome-autor">-->
+<!--                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text-->
+<!--                    </p>-->
+<!--                </div>-->
+<!---->
+<!--                <div class="depoimento-single">-->
+<!--                    <p class="depois-descricao">texto</p>-->
+<!--                    <p class="nome-autor">-->
+<!--                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text-->
+<!--                    </p>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </section>-->
+
+    <footer >
+       <div class="center">
+           <p>
+               Todos os direitos reservados
+           </p>
+       </div>
+    </footer>
+
+</body>
+</html>
